704b8925a96b5bfef9e1b14f0a2f836c1d7d061b
tmair@anonyome.com <tmair@anonyome.com>
2021-12-13 15:20:13 -0600

    try this

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
-import React from "react";
-
+//import React from "react";
+const React = require("react");
 class Component extends React.Component {
   componentDoneDidMount(...args) {
     super.componentDidMount(...args);
   }
 }
 
-export default {
+// export default {
+//   ...React,
+//   Component,
+// };
+
+mopdule.exports = {
   ...React,
   Component,
 };
